0e054ed12399e8ddc827a8a8f36fd5c67e171bc7
黄良运 <1136513099@qq.com>
2019-03-06 11:09:17 +0800

    basic-struts2

---
+++ basic-struts2/src/main/java/com/hly/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.hly.controller;
+
+/**
+ * @author :hly
+ * @github :https://github.com/huangliangyun
+ * @blog :http://www.javahly.com/
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @date :2019/3/6
+ */
+public class IndexController {
+    
+}
