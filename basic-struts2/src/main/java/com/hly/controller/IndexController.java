1c08c6e516cd7ad80f559cd0b5b80d101840a29c
黄良运 <1136513099@qq.com>
2019-03-07 16:12:05 +0800

    SSH

--- basic-struts2/src/main/java/com/hly/controller/IndexController.java
+++ basic-struts2/src/main/java/com/hly/controller/IndexController.java
@@ -8,5 +8,25 @@ package com.hly.controller;
  * @date :2019/3/6
  */
 public class IndexController {
-    
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String execute() {
+        if (!username.equals("123") && !password.equals("123"))
+            return "error";
+        return "success";
+    }
 }
